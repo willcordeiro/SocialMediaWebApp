2d47a2eef4ffcb06e63dc2060bc29f00ca5ffca7
willcordeiro <willcordeiro800@gmail.com>
2022-08-11 15:03:06 -0300

    added right side data

--- src/components/rightSection/RightSection.jsx
+++ src/components/rightSection/RightSection.jsx
@@ -1,25 +1,54 @@
 import React from "react";
-
+import { AiOutlineEdit } from "react-icons/ai";
+import { MdOutlineSearch } from "react-icons/md";
+import Input from "../input/Input";
+import { requests } from "./data/data";
+import { messages } from "./data/data";
 function RightSection() {
   return (
-    <div>
-      <div>
-        Contrary to popular belief, Lorem Ipsum is not simply random text. It
-        has roots in a piece of classical Latin literature from 45 BC, making it
-        over 2000 years old. Richard McClintock, a Latin professor at
-        Hampden-Sydney College in Virginia, looked up one of the more obscure
-        Latin words, consectetur, from a Lorem Ipsum passage, and going through
-        the cites of the word in classical literature, discovered the
-        undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33
-        of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by
-        Cicero, written in 45 BC. This book is a treatise on the theory of
-        ethics, very popular during the Renaissance. The first line of Lorem
-        Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section
-        1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is
-        reproduced below for those interested. Sections 1.10.32 and 1.10.33 from
-        "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their
-        exact original form, accompanied by English versions from the 1914
-        translation by H. Rackham.
+    <div className="right">
+      <div className="messages">
+        <div className="heading">
+          <h4>Messages</h4>
+          <i>
+            <AiOutlineEdit />
+          </i>
+          <div className="search-bar">
+            <i>
+              <MdOutlineSearch />
+              <Input
+                type="search"
+                placeholder="Search messages"
+                id="message-search"
+              />
+            </i>
+          </div>
+          <div className="category">
+            <h6 className="active">Primary</h6>
+            <h6 className="">General</h6>
+            {requests.map((item, index) => (
+              <div key={index}>
+                {" "}
+                <h6>Requests({item.requestNumber})</h6>
+              </div>
+            ))}
+          </div>
+
+          <div className="message">
+            {messages.map((item, index) => (
+              <div>
+                <div className="profile-photo" key={index}>
+                  <img src={item.userImage} />
+                </div>
+
+                <div className="message-body">
+                  <h5>{item.userName}</h5>
+                  <p className="text-muted">{item.UserMessage}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/rightSection/data/data.js
@@ -0,0 +1,14 @@
+export const requests = [{ requestNumber: "7" }];
+
+export const messages = [
+  {
+    userName: "Edem Quist",
+    UserMessage: "just woke up bruh",
+    userImage:
+      "https://i.pinimg.com/originals/a2/5a/89/a25a89c12e0fd087eae85fb348ca553c.jpg",
+  },
+  { userName: "Edem Quist", UserMessage: "just woke up bruh", userImage: "" },
+  { userName: "Edem Quist", UserMessage: "just woke up bruh", userImage: "" },
+  { userName: "Edem Quist", UserMessage: "just woke up bruh", userImage: "" },
+  { userName: "Edem Quist", UserMessage: "just woke up bruh", userImage: "" },
+];
