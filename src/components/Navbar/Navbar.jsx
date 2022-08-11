e1f9e1c63f321a1023d7dde0371e06d899769f62
willcordeiro <willcordeiro800@gmail.com>
2022-08-11 00:50:52 -0300

    fixed navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,9 +9,8 @@ function Navbar() {
       <div className="navbar">
         <h2 className="log">SocialMedia</h2>
         <div className="search-bar">
-          <i>
-            <MdOutlineSearch />
-          </i>
+          <MdOutlineSearch />
+
           <Input
             type="search"
             placeholder="Search for creators, inspirations, and projects"
@@ -19,9 +18,9 @@ function Navbar() {
         </div>
         <div className="create">
           <Button forButton="create-post">New Post</Button>
-        </div>
-        <div className="profile-photo">
-          <img src="https://i1.sndcdn.com/avatars-co78lxGHlvdiOCIN-22MCCg-t500x500.jpg" />
+          <div className="profile-photo">
+            <img src="https://i1.sndcdn.com/avatars-co78lxGHlvdiOCIN-22MCCg-t500x500.jpg" />
+          </div>
         </div>
       </div>
     </>
--- src/components/sidebarProfileMenu/SidebarProfileMenu.jsx
+++ src/components/sidebarProfileMenu/SidebarProfileMenu.jsx
@@ -6,69 +6,65 @@ import { Notification } from "./Data/Data";
 function SidebarProfileMenu() {
   return (
     <>
-      <div className="conta">
-        <div className="left">
-          <a className="profile">
-            <div className="profile-photo">
-              <img src="https://cdn.dribbble.com/users/454765/screenshots/5825882/penguin_32x32px_400x300_new_still.gif?compress=1&resize=400x300" />
-            </div>
-            <div className="handle">
-              <h4>test</h4>
-              <p className="text-muted">@test</p>
-            </div>
-          </a>
-          <div className="sidebar">
-            {SidebarItems.map((item, index) => (
-              <div key={index}>
-                <a
-                  className={`menu-item ${item.activeItem}`}
-                  id={item.NavigationID}
-                >
-                  <i>
-                    <small
-                      className={
-                        item.notificationsCount != ""
-                          ? " notification-count"
-                          : ""
-                      }
-                    >
-                      {item.notificationsCount}
-                    </small>
-                    {item.icon}
-
-                    <h3 className="siderbarItem">{item.text}</h3>
-                  </i>
-                  {item.notificationBar ? (
-                    <div className="backgroundnotifications">
-                      {Notification.map(
-                        (Notificationitem, Notificationindex) => (
-                          <div
-                            className="notifications-popup"
-                            key={Notificationindex}
-                          >
-                            <div className="profile-photo">
-                              <img src={Notificationitem.userProfilePic} />
-                            </div>
-                            <div className="notification-body">
-                              <b>{Notificationitem.userNotificationName}</b>
-                              {Notificationitem.notificationInfo}
-                              <small className="text-muted">
-                                {Notificationitem.notificationTime}
-                              </small>
-                            </div>
-                          </div>
-                        )
-                      )}
-                    </div>
-                  ) : (
-                    <div></div>
-                  )}
-                </a>
-              </div>
-            ))}
+      <div className="left">
+        <a className="profile">
+          <div className="profile-photo">
+            <img src="https://cdn.dribbble.com/users/454765/screenshots/5825882/penguin_32x32px_400x300_new_still.gif?compress=1&resize=400x300" />
           </div>
-          <Button forButton="create-post">Create post</Button>
+          <div className="handle">
+            <h4>test</h4>
+            <p className="text-muted">@test</p>
+          </div>
+        </a>
+        <div className="sidebar">
+          {SidebarItems.map((item, index) => (
+            <a
+              className={`menu-item ${item.activeItem}`}
+              id={item.NavigationID}
+              key={index}
+            >
+              <i>
+                <small
+                  className={
+                    item.notificationsCount != "" ? " notification-count" : ""
+                  }
+                >
+                  {item.notificationsCount}
+                </small>
+                {item.icon}
+
+                <h3 className="siderbarItem">{item.text}</h3>
+              </i>
+              {item.notificationBar ? (
+                <div className="backgroundnotifications">
+                  {Notification.map((Notificationitem, Notificationindex) => (
+                    <div
+                      className="notifications-popup"
+                      key={Notificationindex}
+                    >
+                      <div>
+                        <div className="profile-photo">
+                          <img src={Notificationitem.userProfilePic} />
+                        </div>
+                        <div className="notification-body">
+                          <b>{Notificationitem.userNotificationName}</b>
+                          {Notificationitem.notificationInfo}
+                          <small className="text-muted">
+                            {Notificationitem.notificationTime}
+                          </small>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              ) : (
+                ""
+              )}
+            </a>
+          ))}
         </div>
+
+        <Button forButton="create-post">Create post</Button>
       </div>
     </>
   );
