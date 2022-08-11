b9d796bd8b72b7680063ff5ea4f0532bf1243869
willcordeiro <willcordeiro800@gmail.com>
2022-08-10 23:45:30 -0300

    stories add

--- src/components/Stories/Stories.jsx
+++ src/components/Stories/Stories.jsx
@@ -1,37 +1,40 @@
 import React from "react";
 import { Storie } from "./data/data";
-import Input from "../input/Input";
+import "./Stories.css";
 function Stories() {
   return (
     <>
-      <div className="Stories">
-        <div className="story">
+      <div className="middle">
+        <div className="stories">
           {Storie.map((item, index) => (
-            <div>
-              <div className="profile-photo" key={index}>
-                <img src={item.StorieImage} />
+            <div className="story">
+              <div key={index}>
+                <div className="profile-photo">
+                  <img src={item.StorieImage} />
+                </div>
               </div>
               <p className="name">{item.StorieName}</p>
             </div>
           ))}
         </div>
+        <form className="create-post">
+          <div className="profile-photo">
+            <img src="https://i.pinimg.com/originals/d5/db/75/d5db75268064ba7846ad4d243cdc275b.jpg" />
+          </div>
+          <input
+            type="text"
+            placeholder="What's on your mind, user?"
+            id="create-post"
+            className="btn btn-primary"
+          />
+          <input
+            type="submit"
+            id="create-post"
+            value="Post"
+            className="btn btn-primary"
+          />
+        </form>
       </div>
-      <form className="create-post">
-        <div className="profile-photo">
-          <img src="https://i.pinimg.com/originals/d5/db/75/d5db75268064ba7846ad4d243cdc275b.jpg" />
-        </div>
-        <input
-          type="text"
-          placeholder="What's on your mind, user?"
-          id="create-post"
-        />
-        <input
-          type="submit"
-          id="create-post"
-          value="Post"
-          className="btn btn-primary"
-        />
-      </form>
     </>
   );
 }
